5d1a4f2c2293fc773152d3d8bb335f5d265c0aa7
天知いのり <65227082+AmachiInori@users.noreply.github.com>
2020-09-19 01:49:24 +0800

    优化了文件结构

---
+++ Satoru InoriComplier/lexciTest.cpp
@@ -0,0 +1,11 @@
+#include "lexical.hpp"
+
+int main(){
+    lexAna proc("C:\\Users\\Zhao Yufeng\\Documents\\VSCode\\C++\\Satoru InoriComplier\\Satoru InoriComplier\\lexciTest.sau");
+    token* t = proc.getNextToken();
+    while (t->getTokenType() != 0) {
+        t->printToken();
+        t = proc.getNextToken();
+    }
+    return 0;
+}
---
+++ Satoru InoriComplier/lexical.hpp
@@ -0,0 +1,5 @@
+#ifndef STR_LEXICAL
+#define STR_LEXICAL
+#include "Lexci/DFA.h"
+#include "Lexci/lex.h"
+#endif
